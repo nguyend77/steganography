09bdb4dd0ffb93c49820a563cfb050dd0e7f474c
Dũng Nguyễn <nguyen2023d@gmail.com>
2025-07-19 21:58:05 -0500

    updated supporting file

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import features
 
 def main_program():
 	header = 'Python Steganography Tool'
-	print(f'\n{header.center(40, "*")}\n')
-	path = input('Enter image: ')
+	print(f'\n\n{header.center(40, "-")}')
+	path = input('\nEnter image: ')
 	image = features.Img(path)
-	confirm = input('Confirm that key.py is ready (y/n): ')
+	confirm = input('\nConfirm that key.py is ready (y/n): ')
 	if confirm.lower() == 'y':
 		while not features.check_key():
 			print('Invalid key! Please try again.')
 			input('Adjust key.py and press y to try again\n')
-	print('Choose your desired action:')
+	print('\nChoose your desired action:')
 	print('1. Encrypt hidden message')
 	print('2. Decrypt hidden message')
 	option = input()
 	if option == '1':
-		print('Type message to encrypt in image:')
+		print('\nType message to encrypt in image:')
 		text = input()
-		print('Encrypting message into image...')
+		print('\nEncrypting message into image...')
 		image.encrypt(text)
-		print('Encryption complete. See encrypted.png file in working directory.')
+		print('\nEncryption complete.')
+		print('See encrypted.png file in working directory.\n')
 	elif option == '2':
-		print('Decrypting hidden message from image...')
+		print('\nDecrypting hidden message from image...')
 		image.decrypt()
-		print('Decryption complete. See decrypted.txt file in working directory.')
+		print('\nDecryption complete.')
+		print('See decrypted.txt file in working directory.\n')
 
 # main
 if __name__ == '__main__':
